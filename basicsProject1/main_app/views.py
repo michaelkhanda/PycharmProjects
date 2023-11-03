b35ad361d0e7edf3f0ed49b56117d4d7c0de3250
Michael Khanda <michaelkhanda006@gmail.com>
2023-11-03 20:18:57 +0300

    updated commit

--- basicsProject1/main_app/views.py
+++ basicsProject1/main_app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+from main_app.users import people
+
+
 # Create your views here.
 def home_page(request):
-    return render(request, 'index.html')
+    name = "Jerry Springer"
+    age = 18
+    users = people
+
+    data = {
+        "name": name,
+        'age': age,
+        'users': users
+    }
+    return render(request, 'index.html', context=data)
