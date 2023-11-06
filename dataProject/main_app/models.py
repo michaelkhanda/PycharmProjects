f09566552aeccd5ad6e3cde99ead4b440077f8c4
Michael Khanda <michaelkhanda006@gmail.com>
2023-11-06 14:58:21 +0300

    updated commit

--- dataProject/main_app/models.py
+++ dataProject/main_app/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 class Employee(models.Model):
     name = models.CharField(max_length=40)
     email = models.EmailField(unique=True)
     dob = models.DateField(null=True)
     salary = models.DecimalField(max_digits=6, decimal_places=2)
+    disabled = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
