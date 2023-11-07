21f7a066df9f3d94004d715ae054890ae88d1840
Michael Khanda <michaelkhanda006@gmail.com>
2023-11-07 15:20:24 +0300

    updated commit

--- dataProject/main_app/models.py
+++ dataProject/main_app/models.py
@@ -8,6 +8,12 @@ class Employee(models.Model):
     dob = models.DateField(null=True)
     salary = models.DecimalField(max_digits=6, decimal_places=2)
     disabled = models.BooleanField(default=False)
+    profile = models.ImageField(upload_to="employees", null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)  # Once during creation
+    updated_at = models.DateTimeField(auto_now_add=True, null=True)  # Every time on update happens
 
     def __str__(self):
         return self.name
+
+# python manage.py makemigrations
+# python manage.py migrate
