a6dddb8b989811c344a42b91fe2170598100819d
Michael Khanda <michaelkhanda006@gmail.com>
2023-11-08 17:03:42 +0300

    updated commit

--- dataProject/main_app/views.py
+++ dataProject/main_app/views.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
 
 from main_app.app_forms import EmployeeForm
@@ -16,18 +19,29 @@ def home(request):
         form = EmployeeForm()
     return render(request, 'employee.html', {'form': form})
 
+
 # All Employees
 
 # One Employee
 
 
 def all_employees(request):
-    employees = Employee.objects.all().order_by("salary") # SELECT * FROM employees
+    # employees = Employee.objects.all()# SELECT * FROM employees
+    # employees = Employee.objects.all().order_by("salary")
+    # employees = Employee.objects.filter(name__startswith="La").order_by("dob")
+    employees = Employee.objects.filter(name__startswith="La", salary__gt=45000).order_by("dob")
+    employees = Employee.objects.filter(Q(name__contains="la") | Q(salary__gt=70000))
+    today = datetime.today()
+    day = today.day
+    month = today.month
+
+    employees = Employee.objects.filter(Q(name__contains="la") | ~Q(salary__gt=70000))
+    employees = Employee.objects.filter(dob__day=day, dob__month=month)
     return render(request, "all_employees.html", {"employees": employees})
 
 
 def employee_details(request, emp_id):
-    employee = Employee.objects.get(pk=emp_id) # SELECT * FROM employees WHERE id=1
+    employee = Employee.objects.get(pk=emp_id)  # SELECT * FROM employees WHERE id=1
     return render(request, "employee_details.html", {"employee": employee})
 
 
