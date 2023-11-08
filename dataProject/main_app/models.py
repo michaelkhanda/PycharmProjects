134d996b6262adc68ac30b005fbbf1aeb9ea6448
Michael Khanda <michaelkhanda006@gmail.com>
2023-11-08 15:02:17 +0300

    updated commit

--- dataProject/main_app/models.py
+++ dataProject/main_app/models.py
@@ -1,4 +1,17 @@
+import os.path
+import uuid
+
 from django.db import models
+
+
+def unique_img_name(instance, filename):
+    name = uuid.uuid4()
+    print(name)
+
+    ext = filename.split(".")
+    full_name = f"{name}.{ext}"
+
+    return os.path.join('employees', full_name)
 
 
 # Create your models here.
@@ -6,9 +19,9 @@ class Employee(models.Model):
     name = models.CharField(max_length=40)
     email = models.EmailField(unique=True)
     dob = models.DateField(null=True)
-    salary = models.DecimalField(max_digits=6, decimal_places=2)
+    salary = models.DecimalField(max_digits=7, decimal_places=2)
     disabled = models.BooleanField(default=False)
-    profile = models.ImageField(upload_to="employees", null=True)
+    profile = models.ImageField(upload_to=unique_img_name, null=True, default='employees/employee.png')
     created_at = models.DateTimeField(auto_now_add=True, null=True)  # Once during creation
     updated_at = models.DateTimeField(auto_now_add=True, null=True)  # Every time on update happens
 
